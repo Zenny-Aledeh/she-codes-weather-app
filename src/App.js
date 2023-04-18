55c1263319630417b951abcb75de96ad94b18d85
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-04-18 12:15:43 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Zenny's Weather App</h1>
-      <footer>
-        This projected is created by Zeinab Abu and is{" "}
-        <a
-          href="https://github.com/Zenny-Aledeh/she-codes-weather-app"
-          target="_blank"
-        >
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Zenny's Weather App</h1>
+        <footer>
+          This projected is created by Zeinab Abu and is{" "}
+          <a
+            href="https://github.com/Zenny-Aledeh/she-codes-weather-app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
