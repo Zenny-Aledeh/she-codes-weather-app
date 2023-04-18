0fc8b07ac1f96a7be187a3c3301e16beb84c5b0e
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-04-18 12:00:31 +0100

    added git repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Zenny's Weather App</h1>
+      <footer>
+        This projected is created by Zeinab Abu and is{" "}
+        <a
+          href="https://github.com/Zenny-Aledeh/she-codes-weather-app"
+          target="_blank"
+        >
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
