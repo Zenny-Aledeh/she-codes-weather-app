a8054e41deaf214046277e5c98be3763664bf1cc
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-04-25 18:02:25 +0100

    updated 5 day forecast api

---
+++ src/WeatherForecast.js
@@ -0,0 +1,31 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  const apiKey = "3499ef150985eccadd080ff408a018df";
+  let longitude = 40.7;
+  let latitude = 74;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="Forecast-day">Thu</div>
+          <WeatherIcon code="01d" size={34} />
+          <div className="Forecast-temps">
+            <span className="Forecast-temp-max">19°</span>
+            <span className="Forecast-temp-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
